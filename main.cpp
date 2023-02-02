d3ac6e696a08224f822ab1d6da17be94b6ee1f1c
gustavo correia <gustavo.correia@seidor.com.br>
2023-02-02 16:59:42 -0300

    feat: update game

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <SFML/Graphics.hpp>
 #include <memory>
 #include <experimental/random>
+#include <iostream>
 
 int main() {
     std::srand(std::time(nullptr));
@@ -17,36 +18,57 @@ int main() {
 
     sf::Vector2i position_mouse_win;
     sf::Vector2f position_mouse_coord;
+
+    int point = {};
+
+    std::vector<sf::RectangleShape> objs;
+    size_t max_obj = 5;
+    float obj_max_vel = 10.f;
+    float obj_vel = obj_max_vel;
     
-    while (window.isOpen())
-    {
+    while (window.isOpen()) {
         sf::Event event;
-        while (window.pollEvent(event))
-        {
+        while (window.pollEvent(event)){
             if(event.type == sf::Event::Closed) {
                 window.close();
             }
 
             position_mouse_win = sf::Mouse::getPosition(window);
             position_mouse_coord = window.mapPixelToCoords(position_mouse_win);
-        }
 
-        if(sf::Mouse::isButtonPressed(sf::Mouse::Left)){
-            if(object->getGlobalBounds().contains(position_mouse_coord)) {
-                 x = static_cast <float> (std::experimental::randint(10, (int)(window.getSize().x - object->getSize().x)));
-            object->setPosition(x, 10.f);
+            if(objs.size() < max_obj) {
+                if(obj_vel >= obj_max_vel) {
+                    x = static_cast <float> (std::experimental::randint(10, (int)(window.getSize().x - object->getSize().x)));
+                    object->setPosition(x, 0.f);
+                    objs.push_back( *object );
+                    obj_vel = 0.f;
+
+                } else {
+                    obj_vel += 1.f;
+                }
+            }
+            for(int i{}; i < objs.size(); i++) {
+                bool del = false;
+                objs[i].move(0.f, 5.f);
+                if(sf::Mouse::isButtonPressed(sf::Mouse::Left)){
+                    if(objs[i].getGlobalBounds().contains(position_mouse_coord)) {
+                        del = true;
+                        point += 10.f;
+                    }
+                }
+                if(object->getPosition().y > window.getSize().y) {
+                    del = true;
+                }
+                if(del) {
+                    objs.erase(objs.begin() + i);
+                }
             }
         }
-
         object->move(0.f, 1.9f);
-        if(object->getPosition().y > window.getSize().y) {
-            x = static_cast <float> (std::experimental::randint(10, (int)(window.getSize().x - object->getSize().x)));
-            object->setPosition(x, 10.f);
-
-        }
-
         window.clear();
-        window.draw(*object);
+        for (auto &e : objs) {
+            window.draw(e);
+        }
         window.display();
         
     }
